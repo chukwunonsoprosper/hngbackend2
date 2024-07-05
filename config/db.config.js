10052c4b5f4fbe8f9af34585a99dddd4de79dcf6
fazzy12 <kaluifeanyi00@gmail.com>
2024-07-05 16:00:30 +0100

    Refactor Sequelize database configuration to use environment variables

---
+++ config/db.config.js
@@ -0,0 +1,10 @@
+// config/db.config.js
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT
+});
+
+module.exports = sequelize;
