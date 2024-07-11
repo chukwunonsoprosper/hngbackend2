0b066eafb75a164f01426f28dfe0b5ce18042a0b
fazzy12 <kaluifeanyi00@gmail.com>
2024-07-11 17:19:02 +0100

    define general routes

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
