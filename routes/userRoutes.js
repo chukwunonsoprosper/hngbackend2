51b66b04777a876cea7d4fee3cfc8118d93eb762
fazzy12 <kaluifeanyi00@gmail.com>
2024-07-11 14:57:52 +0100

    set up routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getUserById } = require('../controllers/userController');
 const { verifyToken } = require('../utils/authUtils');
 
-// Route to get user by ID
+
 router.get('/:id', verifyToken, getUserById);
 
 module.exports = router;
